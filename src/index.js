08eaf2abff7928557c331a30dd40a6ff8f880e86
VictorNourisson <victor.nourisson@supdevinci-edu.fr>
2025-03-12 11:36:05 +0100

    Add CRUD functionality with modal confirmation for deletion and styling updates

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Connexion from './Authentification/Connexion';
 import Inscription from './Authentification/Inscription';
+import Consultation from './CRUD/Consultation';
+import AjoutModif from './CRUD/AjoutModif';
+import Suppression from './CRUD/Suppression';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +17,9 @@ root.render(
         <Route path="/" element={<Connexion />} />
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/inscription" element={<Inscription />} />
+        <Route path="/consultation" element={<Consultation />} />
+        <Route path="/ajoutModif" element={<AjoutModif />} />
+        <Route path="/suppression" element={<Suppression />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
