f3bad9e8ca5becf479ec1232d6ca76a0b051fb8d
Reda Loukhnati <mohamedredaloukhnati@yahoo.com>
2025-03-12 11:39:05 +0100

    fix url

--- src/HealthCheck/HealthCheck.js
+++ src/HealthCheck/HealthCheck.js
@@ -6,7 +6,7 @@ const [data, setData] = useState(null);
 const [error, setError] = useState(null);
 
 // const backendURL = "http://127.0.0.1:8080/healthcheck"; 
-const backendURL = "https://app-787be4c4-3ac8-43da-a1e2-e869e769344d.cleverapps.io//healthcheck"; 
+const backendURL = "https://app-787be4c4-3ac8-43da-a1e2-e869e769344d.cleverapps.io/healthcheck"; 
 
 useEffect(() => {
     const fetchHealthCheck = async () => {
