fb1a547cdba707052ff434b6b6285636b660733a
Evan Troget <contact.evantroget@gmail.com>
2025-03-13 09:54:21 +0100

    ajout de la page d'accueil avec le style et la logique pour afficher les livres recommandés et populaires

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Connexion from './Authentification/Connexion';
 import Inscription from './Authentification/Inscription';
+import Accueil from './Accueil/Accueil';
 import reportWebVitals from './reportWebVitals';
 
 import HealthCheck from './HealthCheck/HealthCheck';
@@ -13,7 +14,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Connexion />} />
+        <Route path="/" element={<Accueil />} />
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/inscription" element={<Inscription />} />
         <Route path="/healthcheck" element={<HealthCheck />} />
